54650412d1d50eecdbce84c594918d4b80702454
Stephen Smithstone <stephen@smithstone.co.uk>
2013-11-12 21:47:28 +0000

    moved all code from the jsp into a java class that is bundled up and deployed inside of osgi

---
+++ global/global-cq-domain/src/main/java/smithstone/components/forms/ContactFormHandler.java
@@ -0,0 +1,48 @@
+package smithstone.components.forms;
+
+import com.day.cq.mailer.MailService;
+import org.apache.commons.mail.Email;
+import org.apache.commons.mail.EmailException;
+import org.apache.commons.mail.SimpleEmail;
+import org.apache.sling.api.scripting.SlingScriptHelper;
+
+import javax.servlet.http.HttpServletRequest;
+
+public class ContactFormHandler {
+
+    private SlingScriptHelper scriptHelper;
+    private HttpServletRequest request;
+
+    public ContactFormHandler(SlingScriptHelper scriptHelper, HttpServletRequest request) {
+        this.scriptHelper = scriptHelper;
+        this.request = request;
+    }
+
+    private Email createEmail() throws EmailException {
+        SimpleEmail email = new SimpleEmail();
+        StringBuilder builder = new StringBuilder();
+
+        builder.append("Title: " + request.getParameter("title"));
+        builder.append("First Name: " + request.getParameter("firstname"));
+        builder.append("Last Name: " + request.getParameter("lastname"));
+        builder.append("Telephone: " + request.getParameter("telephone"));
+        builder.append("Mobile: " + request.getParameter("mobile"));
+
+        email.setFrom(request.getParameter("email"));
+
+        email.setMsg(builder.toString()); // <3>
+        email.addTo("stephen@smithstone.co.uk");
+        return email;
+    }
+
+    public void sendEmail() {
+
+        try {
+            Email email = createEmail();
+            MailService service = scriptHelper.getService(MailService.class);
+            service.send(email);
+        } catch (EmailException e) {
+            e.printStackTrace(System.err);
+        }
+    }
+}
